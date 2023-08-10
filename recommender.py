7d4d6413f6f48cede9237e821baa0628b8d69492
Unknownempire <pranav.taru.21@gmail.com>
2023-08-10 20:59:37 +0530

    problem in calculate_recommend

--- recommender.py
+++ recommender.py
@@ -21,20 +21,20 @@ class recommend :
     
     # Load browserHistory from CSV file
     def get_browser_history(self):
-        browserHistory = pd.read_csv("browserHistory.csv")
+        browserHistory = pd.read_csv("browseHistory.csv")
         return browserHistory
     
     # Load product_details from CSV file
     def get_products(self):
         products = pd.read_csv('products.csv')
-        productList = products.value.tolist()
+        productList = products.values.tolist()
         numOfProducts = len(productList)
         return products, productList, numOfProducts
     
     # Load correlation_data from CSV file
 
     def get_correlation_data(self):
-        correlationData = pd.read_csv('correlation_data.csv')
+        correlationData = pd.read_csv('correlationData.csv')
         return correlationData
     
     # Now we will give recommendation based on
@@ -49,16 +49,16 @@ class recommend :
     # Calculate weights for the "date" attribute, which gives higher weight to products ordered RECENTLY by the specific user.
     def calculate_date_weights(self,user_id):
         order_history = self.orderDetails.loc[self.orderDetails['uid'] == user_id]
-        date_data = np.zeroes(self.numOfProducts)
-        max_data = 0
-        min_data = 100000000000
+        date_data = np.zeros(self.numOfProducts)
+        max_date = 0
+        min_date = 100000000000
 
         for index,order in order_history.iterrows():
             current_pid = order['pid']
             order_date = order['date']
             order_time_stamp = time.mktime(datetime.datetime.strptime(order_date,'%d/%m/%Y').timetuple())
-            max_date = max(max_data,order_date)
-            min_date = min(min_date,order_date)
+            max_date = max(max_date,order_time_stamp)
+            min_date = min(min_date,order_time_stamp)
         
         for index,order in order_history.iterrows():
             current_pid = order['pid']
@@ -70,9 +70,9 @@ class recommend :
         return date_data
 
     def calculate_brand_weights(self,user_id):
-        order_history = self.orderDetails.loc[self.orderDetails['uid'] == 'user_id']
+        order_history = self.orderDetails.loc[self.orderDetails['uid'] == user_id]
         print(order_history)
-        user_brand_data = np.zeroes(self.numOfProducts)
+        user_brand_data = np.zeros(self.numOfProducts)
         visited = set()
         pid_bought = order_history['pid'].values.tolist()
 
@@ -112,11 +112,33 @@ class recommend :
 
 # #  Call the getSingleProductRecommendation function on the instance
 # # recommender_instance.getSingleProductRecommendation(1)
+    def calculate_correlation_matrix(self,user_id):
+        price_data = self.correlationData['price'].values.tolist()
+        date_data = self.calculate_date_weights(user_id)
+        name_data = self.correlationData['name'].values.tolist()
+        brand_data = self.correlationData['brand'].values.tolist()
+        user_brand_data = self.calculate_brand_weights(user_id)
+
+        totalData=[price_data,date_data,name_data,brand_data,user_brand_data]
+        index=['price','date','name','brand','userBrandPreference']
+        
+        productXAttributes = pd.DataFrame(data=totalData,index=index,columns=self.products['product_name'].values.tolist())
+
+        productXAttributes = productXAttributes.T
+        productXAttributes = productXAttributes.astype(float)
+        productXAttributes = productXAttributes.dropna()
+        productXAttributes.to_csv("TemporaryMatrix.csv",index=False)
+        print(productXAttributes)
+        print(np.corrcoef(productXAttributes))
+
+        corrcoef = np.corrcoef(productXAttributes)
+        return corrcoef
 
     def calculate_product_Recommendation(self,user_id):
         correlation_matrix = self.calculate_correlation_matrix(user_id)
+
         #search browsing history
-        search_history = self.browserHistory.loc[self.browserHistory['uid'] == user_id]['search'].values.tolist()
+        search_history = self.browseHistory.loc[self.browseHistory['uid'] == user_id]['search'].values.tolist()
         similiar_products =[]
         for browsed_item in search_history:
             for product in self.productList:
@@ -132,14 +154,13 @@ class recommend :
             product_corr = self.correlation_matrix[product_id]
             recommend_product = []
             for i in range(0,len(product_corr)):
-                recommend_product.append([product_corr,i])
-            sorted(product_corr,reverse=True)
+                recommend_product.append([product_corr[i],i])
+            recommend_product = sorted(product_corr,reverse=True)
 
             for i in range(0,5):
-                all_products_set.add(recommend_product[i])
+                all_products_set.add(recommend_product[i][1])
             
             all_product_list = []
-            
             for index in all_products_set:
                 all_product_list.append(self.productList[index])
             
@@ -158,27 +179,6 @@ class recommend :
                 print("product = ", product)
             return final_product_list
         
-        def calculate_correlation_matrix(self,user_id):
-            price_data = self.correlationData['price'].values.tolist()
-            date_data = self.calculate_data_weight(user_id)
-            name_data = self.correlationData['name'].values.tolist()
-            brand_data = self.correlationData['brand'].values.tolist()
-            user_brand_data = self.calculate_brand_weight(user_id)
-
-            totalData=[price_data,date_data,name_data,brand_data,user_brand_data]
-            index=['price','date','name','brand','userBrandPreference']
-        
-            productXAttributes = pd.DataFrame(data=totalData,index=index,columns=self.products['product_name'].values.tolist())
-
-            productXAttributes = productXAttributes.T
-            productXAttributes = productXAttributes.astype(float)
-            productXAttributes = productXAttributes.dropna()
-            productXAttributes.to_csv("TemporaryMatrix.csv",index=False)
-            print(productXAttributes)
-            print(np.corrcoef(productXAttributes))
-
-            corrcoef = np.corrcoef(productXAttributes)
-            return corrcoef
 
         # Testing Purpose
         # def getUserOrderHistory(self,userId):
