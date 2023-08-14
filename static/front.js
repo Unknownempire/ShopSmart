5012f0f69e460f2f29089d7a07a10e162f0553f4
Parthkumar507 <parthkumar281202@gmail.com>
2023-08-14 18:56:07 +0530

    Add hover effect and basic date

--- static/front.js
+++ static/front.js
@@ -1,17 +1,18 @@
 let browsed_product = document.getElementById('browsed_product');
 let recommendation = document.getElementById('recommendations');
+let cartButton =document.getElementById('cartButton')
 function fetch_browsing_history () {fetch('http://127.0.0.1:5000/getUserBrowseHistory')
 .then(response => response.json())
 .then(dataArray => {
-    console.log(dataArray)
+    // console.log(dataArray)
     dataArray.forEach(element => {
         // console.log(element);
         let newdiv = document.createElement("div")
         newdiv.className = 'browsed_items'
-        newdiv.innerHTML = `<p>${element[1]}`;
+        newdiv.innerHTML = `<p>${element[1]}</p><p>${element[2]}</p>`;
         browsed_product.append(newdiv);
 
-            console.log(element[1])
+            // console.log(element[1])
     });
 })
 .catch(error => {
@@ -20,21 +21,29 @@ function fetch_browsing_history () {fetch('http://127.0.0.1:5000/getUserBrowseHi
 
 fetch_browsing_history();
 
-console.log("---------------product recommendation------------")
+// console.log("---------------product recommendation------------")
 
 fetch('http://127.0.0.1:5000/productRecommendation') 
     .then(response => response.json())
     .then(dataArray => {
-        console.log(dataArray)
+        // console.log(dataArray)
         dataArray.forEach(element => {
             // console.log(element);
             let newdiv = document.createElement("div")
             newdiv.className = 'recommended_items'
-            newdiv.innerHTML = `<p>${element[1]}`;
+            newdiv.innerHTML = `<p>${element[1]}</p>`;
             recommendations.append(newdiv);
-            console.log(element[1])
+            // console.log(element[1])
         });
     })
     .catch(error => {
         console.log("Error", error)
     });
+
+  
+        browsed_product.addEventListener('mouseover', () => cartButton.style.backgroundColor = "#94cfab")
+        browsed_product.addEventListener('mouseleave', () => cartButton.style.backgroundColor = "white")
+
+        recommendation.addEventListener('mouseover', () => cartButton.style.backgroundColor = "#94cfab")
+        recommendation.addEventListener('mouseleave', () => cartButton.style.backgroundColor = "white")
+
