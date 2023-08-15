5fc122eb5327da8e05517c03ccde89bf6f450f25
Unknownempire <pranav.taru.21@gmail.com>
2023-08-15 16:50:38 +0530

    function for dynamic styling

--- static/front.js
+++ static/front.js
@@ -1,5 +1,5 @@
 let browsed_product = document.getElementById('browsed_product');
-let recommendation = document.getElementById('recommendations');
+let recommendations = document.getElementById('recommendations');
 let cartButton =document.getElementById('cartButton')
 function fetch_browsing_history () {fetch('http://127.0.0.1:5000/getUserBrowseHistory')
 .then(response => response.json())
@@ -9,7 +9,9 @@ function fetch_browsing_history () {fetch('http://127.0.0.1:5000/getUserBrowseHi
         // console.log(element);
         let newdiv = document.createElement("div")
         newdiv.className = 'browsed_items'
-        newdiv.innerHTML = `<p>${element[1]}</p><p>${element[2]}</p>`;
+        styling_div(newdiv);
+        newdiv.innerHTML = `<img src="./images/ ${element[1].jpg}></img>"
+                            <p>${element[1]}</p><p>${element[2]}</p>`;
         browsed_product.append(newdiv);
 
             // console.log(element[1])
@@ -31,8 +33,12 @@ fetch('http://127.0.0.1:5000/productRecommendation')
             // console.log(element);
             let newdiv = document.createElement("div")
             newdiv.className = 'recommended_items'
-            newdiv.innerHTML = `<p>${element[1]}</p>`;
-            recommendations.append(newdiv);
+            styling_div(newdiv);
+            newdiv.innerHTML = `<img src="./images/${element[1]}" height: 100px width: 100px>
+                                <p>${element[1]}</p>
+                                <p> Brand : ${element[3]}</p>
+                                <p>Price : ${element[2]}</p>`;
+            recommendations.appendChild(newdiv);
             // console.log(element[1])
         });
     })
@@ -40,10 +46,10 @@ fetch('http://127.0.0.1:5000/productRecommendation')
         console.log("Error", error)
     });
 
-  
-        browsed_product.addEventListener('mouseover', () => cartButton.style.backgroundColor = "#94cfab")
-        browsed_product.addEventListener('mouseleave', () => cartButton.style.backgroundColor = "white")
-
-        recommendation.addEventListener('mouseover', () => cartButton.style.backgroundColor = "#94cfab")
-        recommendation.addEventListener('mouseleave', () => cartButton.style.backgroundColor = "white")
+function styling_div(div) {
+    div.style.border = '2p solid green';
+    div.style.maxWidth = '500px';
+    div.style.width = '20vw';
+    div.style.height = '20vh'
+}
 
