5b85ba8ad292e7a5e5c234cc3f680eed20da6997
Parthkumar507 <parthkumar281202@gmail.com>
2023-08-16 15:42:49 +0530

    Add webp image fixture

--- static/front.js
+++ static/front.js
@@ -60,6 +60,25 @@ fetch('http://127.0.0.1:5000/productRecommendation')
         // Use the image URL provided by Flask
         img.alt = element[1]+" img";
 
+         // Use the image URL provided by Flask
+        let imageLoaded = false; // Add a flag to track whether the image has loaded successfully
+        img.alt = element[1]+" img";
+        
+        function handleImageError() {
+            if (!imageLoaded) {
+            imageLoaded = true;
+            img.src =`../static/images/` + element[1]+`.webp`
+            console.log("image source change")
+            }
+        }
+        img.addEventListener('error', handleImageError);
+        img.addEventListener('load', () => {
+            imageLoaded = true;
+        });
+        
+
+
+
         newdiv.appendChild(img);
 
 
