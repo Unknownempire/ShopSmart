fd307c15a0d9e9d9e0ed49b22628bf4155922e2f
Unknownempire <pranav.taru.21@gmail.com>
2023-08-17 00:45:28 +0530

    user_based similarity model

---
+++ NewRecommendation.py
@@ -0,0 +1,93 @@
+import pandas as pd
+
+# Load the data with custom separator and handle missing values
+
+# data = pd.read_csv('./collaborative_model/products2.csv', sep=',', na_values=['', ' ', 'nan', 'NAN'])
+
+# # Drop rows with missing 'uid' or 'productID' values
+# data = data.dropna(subset=['uid', 'productID'])
+
+# # Convert 'productID' column to a list of strings
+# # data['productID'] = data['productID'].apply(lambda x: [item.strip() for item in x.split("-") if item.strip()])
+# data['productID'] = data['productID'].apply(lambda x: [f'{item.strip()}' for item in x.split(" ") if item.strip()])
+
+
+# # Create a list of unique product names
+# all_products = set(item for sublist in data['productID'] for item in sublist)
+
+# # Create a pivot table with product names as columns
+# pivot_table = pd.DataFrame(columns=all_products, index=data['uid'])
+# for idx, row in data.iterrows():
+#     products = row['productID']
+#     pivot_table.loc[row['uid'], products] = 1
+
+# pivot_table.fillna(0, inplace=True)
+# # Calculate the correlation between items
+# correlation = pivot_table.corr()
+# # print(correlation)
+
+
+# Function to get recommendations
+
+class user_similarity :
+    def get_Recommendation(customer_id) :
+        data = pd.read_csv('./collaborative_model/products2.csv', sep=',', na_values=['', ' ', 'nan', 'NAN'])
+
+# Drop rows with missing 'uid' or 'productID' values
+        data = data.dropna(subset=['uid', 'productID'])
+
+# Convert 'productID' column to a list of strings
+# data['productID'] = data['productID'].apply(lambda x: [item.strip() for item in x.split("-") if item.strip()])
+        data['productID'] = data['productID'].apply(lambda x: [f'{item.strip()}' for item in x.split(" ") if item.strip()])
+
+
+# Create a list of unique product names
+        all_products = set(item for sublist in data['productID'] for item in sublist)
+
+# Create a pivot table with product names as columns
+        pivot_table = pd.DataFrame(columns=all_products, index=data['uid'])
+        for idx, row in data.iterrows():
+            products = row['productID']
+            pivot_table.loc[row['uid'], products] = 1
+
+        pivot_table.fillna(0, inplace=True)
+# Calculate the correlation between items
+        correlation = pivot_table.corr()
+# print(correlation)
+    # def get_recommendations(customer_id):
+        customer_purchases = pivot_table.loc[customer_id]
+
+    # Calculate the similarity scores
+        similar_items = correlation.dot(customer_purchases)
+    # print(similar_items)
+    
+    # Filter out already purchased items
+        columnID = ['value']
+    # similar_items = similar_items[similar_items.index.isin(customer_purchases.index) == False]
+        df = pd.DataFrame(similar_items, columns=columnID)
+    # print(df)
+        recommendations = df[df['value'] <= 1]
+    # print(recommendations)
+    # print(similar_items) 
+    # Get the recommended items
+        recommended_items = recommendations.sort_values(by='value', ascending=False)
+        ans = recommended_items.drop(columns=['value'])
+        ans = recommended_items[:5]
+
+        # ans = [row[0] for row in ans]
+        print(ans)
+
+        # return recommended_items
+        return ans
+    get_Recommendation(46)
+
+# Example usage
+
+# customer_id = 46
+
+# print("Customer Purchases:")
+# print(pivot_table.loc[customer_id])
+# recommendations = get_recommendations(customer_id)
+# print("Recommendations:")
+# print(recommendations)
+# print(correlation)
