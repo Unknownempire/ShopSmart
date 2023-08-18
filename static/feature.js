f54224e624d678e2a7e11040aff2175dd1860a13
Parthkumar507 <parthkumar281202@gmail.com>
2023-08-19 00:39:30 +0530

    Add Script for Image slider

--- static/feature.js
+++ static/feature.js
@@ -1,4 +1,3 @@
-console.log('hhhhhh')
 document.addEventListener("DOMContentLoaded", function () {
     // Your JavaScript code here
     window.onscroll = function () {
@@ -15,3 +14,23 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 })
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
